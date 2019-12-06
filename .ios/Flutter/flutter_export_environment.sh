95e4295f156204ef8d089cd913f9b2bde80f5a10
suanle <anle.su@ucarinc.com>
2019-12-06 16:18:25 +0800

    flutter module

---
+++ .ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/suanle/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/suanle/Desktop/my_flutter_module"
+export "FLUTTER_TARGET=/Users/suanle/Desktop/my_flutter_module/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
